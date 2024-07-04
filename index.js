9a273e957da4de0cc85408e10f37fbd8fadaa893
Denys Chavez <dvchf26@gmail.com>
2024-07-04 15:56:11 -0400

    Part3 c.Saving data to MongoDB: Exercise 3.16

--- index.js
+++ index.js
@@ -16,31 +16,25 @@ app.use(express.static("dist"));
 app.use(requestLogger);
 app.use(cors());
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.name, error.name === "ValidationError");
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    console.log("====");
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
 
 morgan.token("body", (req) => JSON.stringify(req.body));
-
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
@@ -64,11 +58,12 @@ app.get("/api/persons/:id", (request, response) => {
     })
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-    Person.findByIdAndDelete(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        }).catch(error => next(error))
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (req, res, next) => {
@@ -91,7 +86,10 @@ app.post("/api/persons", (req, res, next) => {
     .catch((error) => next(error));
 });
 
-const PORT = 3001;
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
