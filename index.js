0890a6697c95498039d9c7ac8c57ab36e8580780
Denys Chavez <dvchf26@gmail.com>
2024-07-04 15:50:19 -0400

    Part3 c.Saving data to MongoDB: Exercise 3.15

--- index.js
+++ index.js
@@ -65,11 +65,11 @@ app.get("/api/persons/:id", (request, response) => {
 })
 
 app.delete("/api/persons/:id", (request, response) => {
-    const id = request.params.id;
-    persons = persons.filter((p) => p.id != id)
-    console.log(persons);
-    response.status(204).end
-})
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        }).catch(error => next(error))
+});
 
 app.post("/api/persons", (req, res, next) => {
   const body = req.body;
